ac38f7aef551d6076b6ff348a85b50a1f980b6b1
王野 <1061256709@qq.com>
2016-11-19 16:03:59 +0800

    WangYe类

---
+++ app/src/main/java/lanou/text1119/WangYe.java
@@ -0,0 +1,37 @@
+package lanou.text1119;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by dllo on 16/11/19.
+ */
+
+public class WangYe extends Activity{
+    String age;
+    String getName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this, "toast", Toast.LENGTH_SHORT).show();
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGetName() {
+        return getName;
+    }
+
+    public void setGetName(String getName) {
+        this.getName = getName;
+    }
+
+}
